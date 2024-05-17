b1c246eb02f61c951901f947d6ea8c2b585ba678
jucapeye <jucapeye@gmail.com>
2024-05-17 13:49:12 -0500

    r main

--- src/PartsUnlimited.Mobile/PartsUnlimited.Droid/Properties/AssemblyInfo.cs
+++ src/PartsUnlimited.Mobile/PartsUnlimited.Droid/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PartsUnlimited.Droid")]
+[assembly: AssemblyTitle("PartsUnlimited.Droid")]7777
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
